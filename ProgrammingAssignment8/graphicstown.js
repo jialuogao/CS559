232ec87ec6ff638ce0840dbd9321c75d0a17a264
Unknown <jialuogao980513@gmail.com>
2017-11-30 01:59:02 -0600

    update

--- ProgrammingAssignment8/graphicstown.js
+++ ProgrammingAssignment8/graphicstown.js
@@ -79,7 +79,7 @@ window.onload = function() {
     controls.appendChild(toExamine);
 
     // make some sliders - using my cheesy panels code
-    var sliders = makeSliders([["TimeOfDay",0,24,6,0.03]]);
+    var sliders = makeSliders([["TimeOfDay",0,24,0,0.02]]);
 
     // this could be gl = canvas.getContext("webgl");
     // but twgl is more robust
