b0e52993cba0a3acb259e7cdf8dc9005624a5263
Unknown <jialuogao980513@gmail.com>
2017-12-01 22:10:11 -0600

    small changes

--- ProgrammingAssignment8/Objects/Birdsnest.js
+++ ProgrammingAssignment8/Objects/Birdsnest.js
@@ -93,6 +93,7 @@ var Birdsnest = undefined;
     gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, image);
     gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
     gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
   };
 
   function initTextureThenDraw(gl,texture)
--- ProgrammingAssignment8/Objects/Watercube.js
+++ ProgrammingAssignment8/Objects/Watercube.js
@@ -110,9 +110,9 @@ var Watercube = undefined;
   function LoadTexture(gl,texture){
     gl.bindTexture(gl.TEXTURE_2D, texture);
     gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, image);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
     gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
     gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
-    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
   };
 
   function initTextureThenDraw(gl,texture)
